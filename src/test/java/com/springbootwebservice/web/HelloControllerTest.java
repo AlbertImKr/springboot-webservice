803bf30536c7e76dce39be53a5eabffb42f4ed16
ALbertIM0427 <guang7226961@naver.com>
2023-05-10 20:39:35 +0900

    feat: Add chapter 2

---
+++ src/test/java/com/springbootwebservice/web/HelloControllerTest.java
@@ -0,0 +1,43 @@
+package com.springbootwebservice.web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @DisplayName("hello가 리턴된다")
+    @Test
+    void hello() throws Exception {
+        String hello = "hello";
+
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @DisplayName("helloDto가 리턴된다")
+    @Test
+    void returnHelloDto() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mockMvc.perform(get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+
+    }
+}
